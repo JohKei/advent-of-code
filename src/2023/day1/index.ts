d3981bbc03a489c9ee77f1eadba5415e0492b91d
JohKei <kaebjohann@googlemail.com>
2023-12-08 17:15:57 +0100

    prepare for day1 part2

--- src/2023/day1/index.ts
+++ src/2023/day1/index.ts
@@ -6,29 +6,8 @@ class Day1 extends Day {
 	}
 
 	solveForPartOne(input: string): string {
-		const lines: string[] = input.split("\n");
-		const lineNumbers: number[] = [];
-
-		const getNumbers = (arg: string[]) => {
-			let numbersInArray = arg.filter((character) =>
-				Number(!isNaN(+character))
-			);
-			const getRowNumber = () => {
-				return numbersInArray[0] + numbersInArray[numbersInArray.length - 1];
-			};
-			const rowNumber = getRowNumber();
-			lineNumbers.push(+rowNumber);
-		};
-
-		lines.forEach((row) => {
-			const splittedRow = row.split("");
-			getNumbers(splittedRow);
-		});
-
-		let solution = lineNumbers.reduce((a, b) => a + b);
-
-		return solution.toString();
-	}
+        return getSolution(input)
+    }
 
 	solveForPartTwo(input: string): string {
 		return input;
@@ -36,3 +15,26 @@ class Day1 extends Day {
 }
 
 export default new Day1();
+
+
+const getSolution = (input: string) => {
+	const lines: string[] = input.split("\n");
+	const lineNumbers: number[] = [];
+
+	const getNumbers = (arg: string[]) => {
+		let numbersInArray = arg.filter((character) => Number(!isNaN(+character)));
+		const getRowNumber = () => {
+			return numbersInArray[0] + numbersInArray[numbersInArray.length - 1];
+		};
+		const rowNumber = getRowNumber();
+		lineNumbers.push(+rowNumber);
+	};
+
+	lines.forEach((row) => {
+		const splittedRow = row.split("");
+		getNumbers(splittedRow);
+	});
+
+	let solution = lineNumbers.reduce((a, b) => a + b);
+	return solution.toString();
+};
