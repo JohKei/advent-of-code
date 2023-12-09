021e3ef7ab258a208d7b53116df2d2819f32df13
JohKei <kaebjohann@googlemail.com>
2023-12-09 23:07:48 +0100

    split functions and prepare for day2-2

--- src/2023/day2/index.ts
+++ src/2023/day2/index.ts
@@ -1,106 +1,125 @@
 import { Day } from "../../day";
 
 class Day2 extends Day {
+	constructor() {
+		super(2);
+	}
 
-    constructor(){
-        super(2);
-    }
+	solveForPartOne(input: string): string {
+		return solutionOne(input);
+	}
 
-    solveForPartOne(input: string): string {
-        return solutionOne(input);
-    }
-
-    solveForPartTwo(input: string): string {
-        return input;
-    }
+	solveForPartTwo(input: string): string {
+		return solutionTwo(input);
+	}
 }
 
-export default new Day2;
+export default new Day2();
+
+const splitGames = (input: string) => {
+	return input.split("\n");
+};
+
+const getGameIdNumber = (game: string) => {
+	let gameId = game.slice(0, game.indexOf(":"));
+	let splittedId = gameId.split("");
+	let gameIdNumber = Number(
+		splittedId.filter((character) => !isNaN(+character)).join("")
+	);
+
+	return gameIdNumber;
+};
+
+const getBagGrab = (game: string) => {
+	return game.slice(game.indexOf(":") + 2).split(/; /g);
+};
+
+const getCubeNumber = (hand: string, color: "blue" | "red" | "green") => {
+	let cubes = hand.split(", ");
+    
+	return (
+		Number(
+			cubes
+				.find((cube) => cube.includes(`${color}`))
+				?.split("")
+				.filter((character) => !isNaN(+character))
+				.join("")
+		) ?? 0
+	);
+};
 
 const solutionOne = (input: string) => {
-    // Information: n <= 12 red allowed
-    // Information: n <= 13 green allowed
-    // Information: n <= 14 blue allowed
-    // Information: get the Id of each possible game and sum the Id's 
+	// n-cubes <= 12 red allowed
+	// n-cubes <= 13 green allowed
+	// n-cubes <= 14 blue allowed
+	// get the Id of each possible game and sum the Id's
 
-    let ruleRed = 12
-    let ruleGreen = 13
-    let ruleBlue = 14
+	let ruleRed = 12;
+	let ruleGreen = 13;
+	let ruleBlue = 14;
 
-    let games = input.split('\n')
-    let gameStatuses: GameStatus[] = [];   
-    let solution = 0;
+	let games = splitGames(input);
+	let gameStatuses: GameStatus[] = [];
+	let solution = 0;
 
-    games.forEach((game) => {
-        // Done: getGameNumber
-        let gameId = game.slice(0, game.indexOf(':'))
-        let splittedId = gameId.split('')
-        let gameIdNumber = Number(splittedId.filter((character) => !isNaN(+character)).join(""))
-        // console.log(gameIdNumber)
+	games.forEach((game) => {
+		let gameIdNumber = getGameIdNumber(game);
+		// console.log(gameIdNumber)
 
-        // Done: split bagGrabs in each Game
-        let bagGrabs = game.slice(game.indexOf(':')+2).split(/; /g)
-        // console.log('bagGrabs',bagGrabs)
-        
-        let highestBlue = 0
-        let highestRed = 0
-        let highestGreen = 0
+		let bagGrabs = getBagGrab(game);
+		// console.log('bagGrabs',bagGrabs)
 
-        bagGrabs.forEach((hand) => {
-            let cubes = hand.split(', ')
-            let blue = Number(
-							cubes
-								.find((cube) => cube.includes("blue"))
-								?.split("")
-								.filter((character) => !isNaN(+character))
-								.join("")
-						) ?? 0;
-            let red = Number(
-							cubes
-								.find((cube) => cube.includes("red"))
-								?.split("")
-								.filter((character) => !isNaN(+character))
-								.join("")
-						) ?? 0;
-            let green = Number(
-							cubes
-								.find((cube) => cube.includes("green"))
-								?.split("")
-								.filter((character) => !isNaN(+character))
-								.join("")
-						) ?? 0;
-            if (blue > highestBlue) highestBlue = blue
-            if (red > highestRed) highestRed = red
-            if (green > highestGreen) highestGreen = green
-        })
+		let highestBlue = 0;
+		let highestRed = 0;
+		let highestGreen = 0;
 
-        gameStatuses.push(new GameStatus(gameIdNumber, highestRed, highestGreen, highestBlue))
+		bagGrabs.forEach((hand) => {
+			let blue = getCubeNumber(hand, "blue");
+			let red = getCubeNumber(hand, "red");
+			let green = getCubeNumber(hand, "green");
 
-    })
-    // console.log(gameStatuses)
-    let validGames = gameStatuses.filter((game) => {
-        if (game.blue <= ruleBlue && game.red <= ruleRed && game.green <= ruleGreen) return game
-    })
+			if (blue > highestBlue) highestBlue = blue;
+			if (red > highestRed) highestRed = red;
+			if (green > highestGreen) highestGreen = green;
+		});
 
+		gameStatuses.push(
+			new GameStatus(gameIdNumber, highestRed, highestGreen, highestBlue)
+		);
+	});
+	// console.log(gameStatuses)
+	let validGames = gameStatuses.filter((game) => {
+		if (game.blue <= ruleBlue && game.red <= ruleRed && game.green <= ruleGreen)
+			return game;
+	});
 
-    console.log(validGames)
+	// console.log(validGames);
 
-    validGames.forEach((game) => solution += game.gameId)
+	validGames.forEach((game) => (solution += game.gameId));
 
-    // console.log(games)
-    return solution.toString()
-}
+	// console.log(games)
+	return solution.toString();
+};
+
+const solutionTwo = (input: string) => {
+	return "solutionTwo";
+};
 
 class GameStatus {
-    gameId: number
-    red: number
-    green: number
-    blue: number
+	gameId: number;
+	red: number;
+	green: number;
+	blue: number;
 
-    constructor(gameId: number, highestRed: number, higestGreen: number, highestBlue: number){
-        this.gameId = gameId
-        this.red = highestRed
-        this.green = higestGreen
-        this.blue = highestBlue
-    }
+	constructor(
+		gameId: number,
+		highestRed: number,
+		higestGreen: number,
+		highestBlue: number
+	) {
+		this.gameId = gameId;
+		this.red = highestRed;
+		this.green = higestGreen;
+		this.blue = highestBlue;
+	}
 }
