e06add5e9886b183ab394d06cf7034486212ebca
JohKei <kaebjohann@googlemail.com>
2023-12-09 10:42:14 +0100

    add day1 input2

--- src/2023/day1/index.ts
+++ src/2023/day1/index.ts
@@ -10,19 +10,47 @@ class Day1 extends Day {
     }
 
 	solveForPartTwo(input: string): string {
-		return input;
+		return getSolution(input);
 	}
 }
 
 export default new Day1();
 
+const writtenNumbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
 
 const getSolution = (input: string) => {
+
 	const lines: string[] = input.split("\n");
 	const lineNumbers: number[] = [];
 
-	const getNumbers = (arg: string[]) => {
-		let numbersInArray = arg.filter((character) => Number(!isNaN(+character)));
+
+	const getNumbers = (splittedRow: string[], fullRow: string) => {
+        // Information: for part 2 I need: 
+        // Information: indexOf, lastIndexOf, slice
+
+        let words :number[]= []
+
+        
+        writtenNumbers.forEach((number:string) => {
+            let firstIndex = 0 
+            firstIndex = fullRow.indexOf(number)
+            let lastIndex = 0 
+            lastIndex = fullRow.lastIndexOf(number)
+            
+            let word = fullRow.slice(firstIndex, lastIndex)
+            if(word) console.log(word, 'word...')
+            if (writtenNumbers.includes(word)){
+                words.push(firstIndex, lastIndex)
+                console.log(words)
+            }
+        })
+        // if (words.length){
+        //     console.log(words, fullRow)
+        // }
+
+
+		let numbersInArray = splittedRow.filter((character) => Number(!isNaN(+character)));
 		const getRowNumber = () => {
 			return numbersInArray[0] + numbersInArray[numbersInArray.length - 1];
 		};
@@ -32,7 +60,7 @@ const getSolution = (input: string) => {
 
 	lines.forEach((row) => {
 		const splittedRow = row.split("");
-		getNumbers(splittedRow);
+		getNumbers(splittedRow, row);
 	});
 
 	let solution = lineNumbers.reduce((a, b) => a + b);
