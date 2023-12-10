3d49671e25061b3dea84496d13ad3c245a5e5e98
JohKei <kaebjohann@googlemail.com>
2023-12-10 18:45:54 +0100

    try to debug & get the solution

--- src/2023/day3/index.ts
+++ src/2023/day3/index.ts
@@ -9,18 +9,19 @@ class Day3 extends Day {
 		// valid numbers n-1 , n , n+1
 
 		let regexSymbols = new RegExp("\\W", "g");
-		// let regexRules = new RegExp("[-#$%&*/@+=]", "g");
-        let regexRules = new RegExp("[^a-zA-Z0-9.\\s]", "g");
+		let regexRules = new RegExp("[-#$%&*/@+=]", "g");
+		// let regexRules = new RegExp("[^a-zA-Z0-9.\\s]", "g");
 
 		let lines = input.split("\n");
 
 		let numbersPerLineStrings = lines.map((line) => line.split(regexSymbols).filter((value) => value !== ""));
-
+		let duplicatedNumbers: string[] = [];
 		let allNumbers: InputNumber[] = [];
 
 		lines.forEach((line, indexOfLine) => {
 			numbersPerLineStrings[indexOfLine].forEach((number) => {
 				if (numbersPerLineStrings[indexOfLine].filter((value) => value === number).length > 1) {
+					duplicatedNumbers.push(number);
 					allNumbers.push(new InputNumber(indexOfLine, +number, line.indexOf(number)));
 					allNumbers.push(new InputNumber(indexOfLine, +number, line.lastIndexOf(number)));
 				} else {
@@ -30,31 +31,66 @@ class Day3 extends Day {
 		});
 
 		allNumbers.forEach((number) => {
-			for (let i = number.indexStart === 0 ? 0 : number.indexStart - 1; i <= number.indexEnd + 1; i++) {
-	            let previousLine = number.line === 0 ? null : lines[number.line - 1];
-				let currentLine = lines[number.line];
-				let nextLine = number.line >= allNumbers.length - 1 ? null : lines[number.line + 1];
-				// check previous Line
-				if (previousLine !== null && previousLine !== undefined) {
-					if (regexRules.test(previousLine.charAt(i))) {
+			let startIndex = number.indexStart === 0 ? 0 : number.indexStart - 1;
+			let endIndex = number.indexEnd;
+			let prevLine = number.line === 0 ? null : number.line - 1;
+			let nextLine = number.line === lines.length - 1 ? null : number.line + 1;
+
+			if (regexRules.test(lines[number.line][startIndex])) {
+				number.isValid = true;
+			}
+			if (regexRules.test(lines[number.line][endIndex])) {
+				number.isValid = true;
+				// console.log(lines[number.line]);
+				// console.log(number);
+			}
+			// test previousLine
+			if (prevLine !== null) {
+				for (let i = startIndex; i <= endIndex; i++) {
+					if (regexRules.test(lines[prevLine][i])) {
 						number.isValid = true;
-					}
-				}
-				// Check current Line
-				if (regexRules.test(currentLine.charAt(i))) {
-					number.isValid = true;
-				}
-				// check next Line
-				if (nextLine !== null && nextLine !== undefined) {
-					if (regexRules.test(nextLine.charAt(i))) {
-						number.isValid = true;
+						continue;
 					}
 				}
 			}
+			//test nextLine
+			if (nextLine !== null) {
+				for (let i = startIndex; i <= endIndex; i++) {
+					if (regexRules.test(lines[nextLine][i])) {
+						number.isValid = true;
+						continue;
+					}
+				}
+			}
+
+			console.log("");
+			console.log("");
+			console.log("");
+			console.log("");
+			console.log("");
+			console.log("");
+			if (prevLine) console.log(lines[prevLine]);
+			console.log(lines[number.line]);
+			if (nextLine) console.log(lines[nextLine]);
+			console.log("");
+			if (prevLine) console.log(lines[prevLine].slice(startIndex, endIndex + 1), "prevLine");
+			console.log(lines[number.line].slice(startIndex, endIndex + 1), "currentLine");
+			if (nextLine) console.log(lines[nextLine].slice(startIndex, endIndex + 1), "nextLine");
+			console.log("");
+			// test currentLine
+			console.log("has Duplicate", duplicatedNumbers.includes(number.number.toString()));
+			console.log("is Valid", number.isValid);
+			console.log("");
+
+			console.log(number);
+
+			// if (lines[number.line].slice(startIndex, endIndex) === '.776*'){
+			//     console.log(lines[number.line][endIndex-1])
+			//     console.log(regexRules.test(lines[number.line][endIndex-1]));
+			// }
 		});
 		let solution = 0;
 		allNumbers.filter((number) => number.isValid).forEach((number) => (solution += number.number));
-
 		return solution.toString();
 	}
 
@@ -77,6 +113,6 @@ export class InputNumber {
 		this.number = inputNumber;
 		this.indexStart = indexStart;
 
-		this.indexEnd = indexStart + this.number.toString().length - 1;
+		this.indexEnd = indexStart + this.number.toString().length;
 	}
 }
