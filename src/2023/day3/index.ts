8a446f6e831493e8474650e14dad724cdbbeb95d
JohKei <kaebjohann@googlemail.com>
2023-12-10 16:42:08 +0100

    failed to solve this day

--- src/2023/day3/index.ts
+++ src/2023/day3/index.ts
@@ -6,42 +6,34 @@ class Day3 extends Day {
 	}
 
 	solveForPartOne(input: string): string {
-		// Information: valid numbers n-1 , n , n+1
+		// valid numbers n-1 , n , n+1
 
 		let regexSymbols = new RegExp("\\W", "g");
-        let regexRules = new RegExp("[^a-zA-Z0-9.]", "g");
-		// let regexNumbers = new RegExp("[0-9]", "g");
+		// let regexRules = new RegExp("[-#$%&*/@+=]", "g");
+        let regexRules = new RegExp("[^a-zA-Z0-9.\\s]", "g");
 
 		let lines = input.split("\n");
 
-let numbersPerLineStrings = lines.map((line) =>
-    line.split(regexSymbols).filter((value) => value !== "")
-);
+		let numbersPerLineStrings = lines.map((line) => line.split(regexSymbols).filter((value) => value !== ""));
 
-// Done: I need all numbers
-let allNumbers: InputNumber[] = [];
-		// Done: getIndexOf Number number.length lastIndex of Number
+		let allNumbers: InputNumber[] = [];
+
 		lines.forEach((line, indexOfLine) => {
 			numbersPerLineStrings[indexOfLine].forEach((number) => {
-				allNumbers.push(
-					new InputNumber(indexOfLine, +number, line.indexOf(number))
-				);
+				if (numbersPerLineStrings[indexOfLine].filter((value) => value === number).length > 1) {
+					allNumbers.push(new InputNumber(indexOfLine, +number, line.indexOf(number)));
+					allNumbers.push(new InputNumber(indexOfLine, +number, line.lastIndexOf(number)));
+				} else {
+					allNumbers.push(new InputNumber(indexOfLine, +number, line.indexOf(number)));
+				}
 			});
 		});
 
-		// Done: forEach number.
-
-		// Done: while let i = startIndex, while i <= indexEnd, i++{ check for lines-1, currentLine, lines+1}
 		allNumbers.forEach((number) => {
-			for (
-				let i = number.indexStart === 0 ? 0 : number.indexStart - 1;
-				i <= number.indexEnd + 1;
-				i++
-			) {
-				let previousLine = number.line === 0 ? null : lines[number.line - 1];
+			for (let i = number.indexStart === 0 ? 0 : number.indexStart - 1; i <= number.indexEnd + 1; i++) {
+	            let previousLine = number.line === 0 ? null : lines[number.line - 1];
 				let currentLine = lines[number.line];
-				let nextLine =
-					number.line >= allNumbers.length - 1 ? null : lines[number.line + 1];
+				let nextLine = number.line >= allNumbers.length - 1 ? null : lines[number.line + 1];
 				// check previous Line
 				if (previousLine !== null && previousLine !== undefined) {
 					if (regexRules.test(previousLine.charAt(i))) {
@@ -61,10 +53,8 @@ let allNumbers: InputNumber[] = [];
 			}
 		});
 		let solution = 0;
-		allNumbers
-			.filter((number) => number.isValid)
-			.forEach((number) => (solution += number.number));
-		// console.log(allNumbers, allNumbers.length);
+		allNumbers.filter((number) => number.isValid).forEach((number) => (solution += number.number));
+
 		return solution.toString();
 	}
 
