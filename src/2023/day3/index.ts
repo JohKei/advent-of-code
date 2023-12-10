857e900140fb8a7ba821d3c4a8845b6bfdb723b9
JohKei <kaebjohann@googlemail.com>
2023-12-10 20:45:37 +0100

    fix prevLines-bug

--- src/2023/day3/index.ts
+++ src/2023/day3/index.ts
@@ -6,25 +6,28 @@ class Day3 extends Day {
 	}
 
 	solveForPartOne(input: string): string {
-		// valid numbers n-1 , n , n+1
-
 		let regexSymbols = new RegExp("\\W", "g");
 		let regexRules = new RegExp("[-#$%&*/@+=]", "g");
-		// let regexRules = new RegExp("[^a-zA-Z0-9.\\s]", "g");
 
 		let lines = input.split("\n");
 
-		let numbersPerLineStrings = lines.map((line) => line.split(regexSymbols).filter((value) => value !== ""));
+		let numbersPerLineStrings = lines.map((line) =>
+			line.split(regexSymbols).filter((value) => value !== "")
+		);
 		let duplicatedNumbers: string[] = [];
 		let allNumbers: InputNumber[] = [];
 
 		lines.forEach((line, indexOfLine) => {
+			let numberToSkip: string;
 			numbersPerLineStrings[indexOfLine].forEach((number) => {
-				if (numbersPerLineStrings[indexOfLine].filter((value) => value === number).length > 1) {
-					// console.log('duplicate......', number, line)
-					duplicatedNumbers.push(number);
+				if (numberToSkip === number) {
+					numberToSkip = "";
+				} else if (
+					numbersPerLineStrings[indexOfLine].filter((value) => value === number).length > 1
+				) {
 					allNumbers.push(new InputNumber(indexOfLine, +number, line.indexOf(number)));
 					allNumbers.push(new InputNumber(indexOfLine, +number, line.lastIndexOf(number)));
+					numberToSkip = number;
 				} else {
 					allNumbers.push(new InputNumber(indexOfLine, +number, line.indexOf(number)));
 				}
@@ -34,10 +37,9 @@ class Day3 extends Day {
 		allNumbers.forEach((number) => {
 			let startIndex = number.indexStart === 0 ? 0 : number.indexStart - 1;
 			let endIndex = number.indexEnd;
-			let prevLine = number.line === 0 ? null : number.line - 1;
+			let prevLine = number.line === 1 ? 0 : number.line === 0 ? null : number.line - 1;
 			let nextLine = number.line === lines.length - 1 ? null : number.line + 1;
-
-			if (prevLine) {
+			if (prevLine !== null) {
 				if (regexRules.test(lines[prevLine].slice(startIndex, endIndex + 1))) {
 					number.isValid = true;
 				}
@@ -50,58 +52,27 @@ class Day3 extends Day {
 					number.isValid = true;
 				}
 			}
-			// if (regexRules.test(lines[number.line][startIndex])) {
-			// 	number.isValid = true;
-			// }
-			// if (regexRules.test(lines[number.line][endIndex])) {
-			// 	number.isValid = true;
-			// 	// console.log(lines[number.line]);
-			// 	// console.log(number);
-			// }
-			// // test previousLine
-			// if (prevLine !== null) {
-			// 	for (let i = startIndex; i <= endIndex; i++) {
-			// 		if (regexRules.test(lines[prevLine][i])) {
-			// 			number.isValid = true;
-			// 			continue;
-			// 		}
-			// 	}
-			// }
-			// //test nextLine
-			// if (nextLine !== null) {
-			// 	for (let i = startIndex; i <= endIndex; i++) {
-			// 		if (regexRules.test(lines[nextLine][i])) {
-			// 			number.isValid = true;
-			// 			continue;
-			// 		}
-			// 	}
-			// }
 
-			// 	console.log("");
-			// console.log("");
-			// console.log("");
-			// console.log("");
-			// console.log("");
-			// console.log("");
-			// if (prevLine) console.log(lines[prevLine]);
-			// console.log(lines[number.line]);
-			// if (nextLine) console.log(lines[nextLine]);
-			// console.log("");
-			// if (prevLine) console.log(lines[prevLine].slice(startIndex, endIndex + 1), "prevLine");
-			// console.log(lines[number.line].slice(startIndex, endIndex + 1), "currentLine");
-			// if (nextLine) console.log(lines[nextLine].slice(startIndex, endIndex + 1), "nextLine");
-			// console.log("");
-			// // test currentLine
-			// console.log("has Duplicate", duplicatedNumbers.includes(number.number.toString()));
-			// console.log("is Valid", number.isValid);
-			// console.log("");
+			console.log("");
+			console.log("");
+			console.log("");
+			console.log("");
+			console.log("");
+			console.log('');
+			if (prevLine) console.log(lines[prevLine]);
+			console.log(lines[number.line]);
+			if (nextLine) console.log(lines[nextLine]);
+			console.log("");
+			if (prevLine) console.log(lines[prevLine].slice(startIndex, endIndex + 1), "prevLine");
+			console.log(lines[number.line].slice(startIndex, endIndex + 1), "currentLine");
+			if (nextLine) console.log(lines[nextLine].slice(startIndex, endIndex + 1), "nextLine");
+			console.log("");
+			// test currentLine
+			console.log("has Duplicate", duplicatedNumbers.includes(number.number.toString()));
+			console.log("is Valid", number.isValid);
+			console.log("");
 
-			// console.log(number);
-
-			// if (lines[number.line].slice(startIndex, endIndex) === '.776*'){
-			//     console.log(lines[number.line][endIndex-1])
-			//     console.log(regexRules.test(lines[number.line][endIndex-1]));
-			// }
+			console.log(number);
 		});
 		let solution = 0;
 		allNumbers.filter((number) => number.isValid).forEach((number) => (solution += number.number));
@@ -130,3 +101,35 @@ export class InputNumber {
 		this.indexEnd = indexStart + this.number.toString().length;
 	}
 }
+
+// if (regexRules.test(lines[number.line][startIndex])) {
+// 	number.isValid = true;
+// }
+// if (regexRules.test(lines[number.line][endIndex])) {
+// 	number.isValid = true;
+// 	// console.log(lines[number.line]);
+// 	// console.log(number);
+// }
+// // test previousLine
+// if (prevLine !== null) {
+// 	for (let i = startIndex; i <= endIndex; i++) {
+// 		if (regexRules.test(lines[prevLine][i])) {
+// 			number.isValid = true;
+// 			continue;
+// 		}
+// 	}
+// }
+// //test nextLine
+// if (nextLine !== null) {
+// 	for (let i = startIndex; i <= endIndex; i++) {
+// 		if (regexRules.test(lines[nextLine][i])) {
+// 			number.isValid = true;
+// 			continue;
+// 		}
+// 	}
+// }
+
+// if (lines[number.line].slice(startIndex, endIndex) === '.776*'){
+//     console.log(lines[number.line][endIndex-1])
+//     console.log(regexRules.test(lines[number.line][endIndex-1]));
+// }
